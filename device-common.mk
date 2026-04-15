8bd15465ac71bb4fd5b032390c9c4129c66f060d
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-16 07:18:23 +0800

    using 12gb dalvik heap config
    Change-Id: I274f7f13caaf2e55ab76d81726316429272410b3

--- device-common.mk
+++ device-common.mk
@@ -7,6 +7,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+$(call inherit-product, frameworks/native/build/phone-xhdpi-12288-dalvik-heap.mk)
+
 # A/B
 ifeq ($(WITH_GMS),true)
 PRODUCT_SYSTEM_PARTITIONS_FILE_SYSTEM_TYPE ?= erofs
